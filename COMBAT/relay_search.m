1e63a6e2045d7e3bf6fc429e4bba027bd0f00364
zrcao <zcao@isi.edu>
2015-04-02 18:10:54 -0400

    Work-in-progress @ relay_search.m

--- COMBAT/relay_search.m
+++ COMBAT/relay_search.m
@@ -15,8 +15,11 @@ end
 s_nodes(end) = num_nodes;
 d_nodes(end) = num_nodes-1;
 
-mc = 1000;
+mc = 5000;
 hop1_iterates = zeros(mc, 1);
+hop1_abnormal = zeros(mc, 1);
+hop1_noswitch = zeros(mc, 1);
+
 hop2_iterates = zeros(mc, 1);
 max_hops = zeros(mc, 1);
 for mm = 1:mc
@@ -32,26 +35,60 @@ for mm = 1:mc
     full_UG = full(UG);
 
     iter = 0;
+    abnorm = 0;
+    noswitch = 0;
     old_relay_pool =[];
     hop1_converged = 0;
     while ~hop1_converged
         iter = iter+1;
         [dist,path,pred] = graphshortestpath(UG,1,'directed',false);
         relay_pool = find(pred==1);
-        if isequal(relay_pool, old_relay_pool)
+        if ismember(relay_pool, old_relay_pool) 
+            % If the new search results in a set of potential first hops
+            % that is a subset of the previous set of first hops. The
+            % search for relay hops stops.
             hop1_converged = 1;
-        else        
+        else
             num_relay = length(relay_pool);
             val = zeros(num_relay, 1);
             for ii = 1:num_relay
                 val(ii) = full_UG(1, relay_pool(ii));
             end
-            new_weight=max(val);
-            if min(val)== new_weight
+            [maxDelay, pos] = max(val);
+            maxDelayRelay = relay_pool(pos);
+            if min(val)== maxDelay
+                % If all first hop relays have the same rates, the search
+                % is done.
                 hop1_converged=1;
             else
-                for ii = 1:num_relay
-                    DG(relay_pool(ii), 1) = new_weight; 
+                % Find the longest path. maxpath(2) is the first hop of the
+                % longest path.
+                [maxdist, maxdistNode] = max(dist);
+                maxpath = graphpred2path(pred, maxdistNode);
+                
+                if full_UG(1, maxpath(2))~=maxDelay
+                    abnorm = abnorm+1;
+                    % If the longest path's first hop does not have the 
+                    % lowest rate (i.e., maxDelay) among all first hop relays,
+                    % Check whether the relay hop with the lowest rate can
+                    % use maxpath(2) as its relay. It can as long as the
+                    % longest delay path doesn't change or the new longest
+                    % path has less penalty.
+                    DG_tmp = DG;
+                    DG_tmp(maxDelayRelay, 1) = 10;
+                    UG_tmp = DG_tmp'+DG_tmp;
+                    [dist_tmp,path_tmp,pred_tmp] = ...
+                        graphshortestpath(UG_tmp,1,'directed',false);
+                    if max(dist_tmp)<=maxdist
+                        DG(maxDelayRelay, 1) = 10;
+                    else
+                        noswitch = noswitch + 1;
+                    end
+                else
+                    new_weight = maxDelay;
+                    for ii = 1:num_relay
+                        DG(relay_pool(ii), 1) = new_weight; 
+                    end
                 end
                 old_relay_pool = relay_pool;
                 UG = DG+DG';
@@ -60,15 +97,15 @@ for mm = 1:mc
         end
     end
     hop1_iterates(mm) = iter;
+    hop1_abnormal(mm) = abnorm;
+    hop1_noswitch(mm) = noswitch;
     
     paths = graphpred2path(pred, [2:num_nodes]);
     pathhops = zeros(num_nodes-1, 1);
     for ii = 1:num_nodes-1
         pathhops(ii) = length(paths{1})-1;
     end
-    max_hops(mm) = max(pathhops);
-    
-    
+    max_hops(mm) = max(pathhops);  
 end
     
     hop2_converged = 0;
