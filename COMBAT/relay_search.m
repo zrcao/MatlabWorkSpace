340b2dcf5a79c7e43a709037d7206d94bc183cc6
Zhongren Cao <zrcaoxs@gmail.com>
2015-04-04 13:25:11 -0400

    work-in-progress

--- COMBAT/relay_search.m
+++ COMBAT/relay_search.m
@@ -1,5 +1,5 @@
 clear *;
-test = 1;
+test = 0;
 
 rates = [6 9 12 18 24 36 48 54];
 num_rates = length(rates);
@@ -21,10 +21,13 @@ d_nodes(end) = num_nodes-1;
 mc = 5000;
 hop1_iterates = zeros(mc, 1);
 hop1_noswitch = zeros(mc, 1);
-
 hop2_iterates = zeros(mc, 1);
 max_hops = zeros(mc, 1);
-for mm = 1:mc
+max_dist = zeros(mc, 1);
+num_bins = 6;
+num_hops = zeros(mc, num_bins);
+
+for mm = 1:mc 
     idx = randi([1, num_rates], num_nodes*(num_nodes-1)/2, 1);
     weights = delay(idx);
 
@@ -134,119 +137,96 @@ for mm = 1:mc
             DG(nn, 1) = 10;
         end
     end
-   
     
     iter =0;
     hop2_converged = 0;
     old_hops = [];
     while ~hop2_converged && ~finished
         iter = iter+1;
-        hops = [];
-        val = [];
-        for rr = 1:num_relay
-            node_pool = find(pred==relay_pool(rr));
-            num_thirds = length(node_pool);
-            for tt = 1:num_thirds
-                zishop = [relay_pool(rr) node_pool(tt)];
-                hops = [hops; zishop];
-                val = [val; full_UG(zishop(1), zishop(2))];
-            end
-        end
-        [max23Delay, pairIdx] = max(val);
-        % check whether the maximum delay hop is between 9 and 10;
-        special = 0;
-        if isequal(hops(pairIdx, :), [9, 10]) || isequal(hops(pairIdx, :), [10, 9])
-            special = 1;
-        end
         
-        if min(val)==max23Delay
-            hop2_converged = 1;
+        [dist,route,pred] = graphshortestpath(DG,1,'directed',false);
+        [maxdist, maxdistNode] = max(dist);
+        %maxpath = graphpred2path(pred, maxdistNode);
+        maxpath = route{maxdistNode};
+        if length(maxpath)<3
+            finished = 1;
         else
-            maxpath23Delay = full_UG(maxpath(2), maxpath(3));
-            copy_weight = 0;
-            if maxpath23Delay~=max23Delay
-                DG_tmp = DG;
-                DG_tmp(hops(pairIdx, 2), 1) = 10;
-                if ~special
-                    DG_tmp(max(hops(pairIdx, :)), min(hops(pairIdx, :))) = 10;
-                else
-                    DG_tmp(9, 10) = 10;
+            hops = [];
+            val = [];
+            for rr = 1:num_relay
+                node_pool = find(pred==relay_pool(rr));
+                num_thirds = length(node_pool);
+                for tt = 1:num_thirds
+                    zishop = [relay_pool(rr) node_pool(tt)];
+                    hops = [hops; zishop];
+                    val = [val; full_UG(zishop(1), zishop(2))];
                 end
-                [dist_tmp,route_tmp,pred_tmp] = ...
-                        graphshortestpath(DG_tmp,1,'directed',false);
-                altermaxdist = maxdist + max23Delay-maxpath23Delay;
-                
-                if (max(dist_tmp)<=altermaxdist)
-                    DG = DG_tmp;
+            end
+            [max23Delay, pairIdx] = max(val);
+            % check whether the maximum delay hop is between the last two
+            % nodes. The way the undirect graph is setup in Matlab requires we
+            % to swap the index of the edge between these two nodes.
+            cond = (isequal(hops(pairIdx, :), [num_nodes-1, num_nodes])) || ...
+                (isequal(hops(pairIdx, :), [num_nodes, num_nodes-1]));
+            special = 0;
+            if cond
+                special = 1;
+            end
+
+            if min(val)==max23Delay
+                hop2_converged = 1;
+            else
+                maxpath23Delay = full_UG(maxpath(2), maxpath(3));
+                copy_weight = 0;
+                if maxpath23Delay~=max23Delay
+                    DG_tmp = DG;
+                    %DG_tmp(hops(pairIdx, 2), 1) = 10;
+                    if ~special
+                        DG_tmp(max(hops(pairIdx, :)), min(hops(pairIdx, :))) = 10;
+                    else
+                        DG_tmp(num_nodes-1, num_nodes) = 10;
+                    end
+                    [dist_tmp,route_tmp,pred_tmp] = ...
+                            graphshortestpath(DG_tmp,1,'directed',false);
+                    altermaxdist = maxdist + max23Delay-maxpath23Delay;
+
+                    if (max(dist_tmp)<=altermaxdist)
+                        DG = DG_tmp;
+                    else
+                        copy_weight = 1;
+                    end
                 else
                     copy_weight = 1;
                 end
-            else
-                copy_weight = 1;
-            end
-            
-            if copy_weight
-                num_pairs = length(val);
-                for pp = 1:num_pairs
-                    if isequal(hops(pp, :), [9, 10]) || isequal(hops(pp, :), [10, 9])
-                        DG(9,10) = max23Delay;
-                    else
-                        DG(max(hops(pp, :)), min(hops(pp, :))) = max23Delay;
+
+                if copy_weight
+                    num_pairs = length(val);
+                    for pp = 1:num_pairs
+                        cond = (isequal(hops(pp, :), [num_nodes-1, num_nodes])) ...
+                            || (isequal(hops(pp, :), [num_nodes, num_nodes-1]));
+                        if cond
+                            DG(num_nodes-1,num_nodes) = max23Delay;
+                        else
+                            DG(max(hops(pp, :)), min(hops(pp, :))) = max23Delay;
+                        end
                     end
                 end
+                old_hops = hops;
+                UG = DG+DG';
+                full_UG = full(UG);
             end
-            old_hops = hops;
-            UG = DG+DG';
-            full_UG = full(UG);
-        end
-        [dist,route,pred] = graphshortestpath(DG,1,'directed',false);
-        [maxdist, maxdistNode] = max(dist);
-        maxpath = graphpred2path(pred, maxdistNode);
-        if length(maxpath)<3
-            finished = 1;
         end
     end
-   
+    hop2_iterates(mm) = iter;
     
-    
-%     %% Dealing with the second hop
-%     
-%     hop2_converged = 0;
-%     while ~hop2_converged
-%         hop2_nodes = find(pred~=1 & pred~=0);
-%         num_hop2_nodes = length(hop2_nodes);
-%         relays = [];
-%         for hh = 1:num_hop2_nodes;
-%             if ~ismember(pred(hop2_nodes(hh)), relays)
-%                 relays = [relays; pred(hop2_nodes(hh))];
-%             end
-%         end
-%         relays = sort(relays);
-%         
-%         val = zeros(num_hop2_nodes, 1);
-%         for ii = 1:hop2_nodes
-%             idx1 = min(hop2_nodes(ii), pred(hop2_nodes(ii)));
-%             idx2 = max(hop2_nodes(ii), pred(hop2_nodes(ii)));
-%             val(ii) = DG(idx1, idx2);
-%         end
-%         new_weight=max(val);
-%         if min(val)== new_weight
-%             hop2_converged=1;
-%         else
-%             for ii = 1:num_relay
-%                 idx1 = min(hop2_nodes(ii), pred(hop2_nodes(ii)));
-%                 idx2 = max(hop2_nodes(ii), pred(hop2_nodes(ii)));
-%                 DG(idx1, idx2) = new_weight; 
-%             end
-%         end
-%     end
-    
-    paths = graphpred2path(pred, [2:num_nodes]);
     pathhops = zeros(num_nodes-1, 1);
     for ii = 1:num_nodes-1
-        pathhops(ii) = length(paths{1})-1;
+        pathhops(ii) = length(route{ii+1})-1;
     end
-    max_hops(mm) = max(pathhops);  
+    num_hops(mm, :) = hist(pathhops, 1:num_bins);
+    
+    max_hops(mm) = max(pathhops);
+    max_dist(mm) = max(maxdist);
 end
 
 
